ad61fba33a74eb09c89e1d3a168002b515870c4d
nickthady <galvin.nicholas22i@egressy.info>
2026-05-21 13:46:30 +0200

    After screenshot hozzáadva, minden teszt zöld

--- BankAccountApp/Program.cs
+++ BankAccountApp/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace BankAccountApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using BankAccountApp;
+
+var account = new BankAccount("Alice", 200);
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Tulajdonos: {account.GetOwner()}");
+Console.WriteLine($"Egyenleg: {account.GetBalance()} Ft");
+Console.WriteLine($"Utolsó tranzakció: {account.GetHistory().GetLastTransaction()}");
+Console.WriteLine($"Összes befizetés: {account.GetHistory().GetTotalDeposited()} Ft");
+Console.WriteLine($"Összes kivétel: {account.GetHistory().GetTotalWithdrawn()} Ft");
--- BankAccountAppTests/Tests.cs
+++ BankAccountAppTests/Tests.cs
@@ -3,7 +3,7 @@
 namespace BankAccountAppTests
 {
     [TestClass]
-    public  class BankAccountTests
+    public class BankAccountTests
     {
         private BankAccount CreateDefaultAccount() => new BankAccount("Alice", 100);
 
@@ -14,8 +14,27 @@ namespace BankAccountAppTests
             Assert.AreEqual("Alice", account.GetOwner());
             Assert.AreEqual(100, account.GetBalance());
         }
-        // TODO: üres/null owner -> ArgumentException
-        // TODO: negatív initialBalance -> ArgumentException
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NullOwner_ThrowsArgumentException()
+        {
+            var account = new BankAccount(null, 100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_EmptyOwner_ThrowsArgumentException()
+        {
+            var account = new BankAccount("", 100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NegativeInitialBalance_ThrowsArgumentException()
+        {
+            var account = new BankAccount("Alice", -50);
+        }
 
         [TestMethod]
         public void Deposit_ValidAmount()
@@ -25,9 +44,33 @@ namespace BankAccountAppTests
             Assert.IsTrue(result);
             Assert.AreEqual(150, account.GetBalance());
         }
-        // TODO: amount = 0 -> false
-        // TODO: negatív amount -> false
-        // TODO: több egymás utáni deposit helyes összeget ad-e
+
+        [TestMethod]
+        public void Deposit_ZeroAmount_ReturnsFalse()
+        {
+            var account = CreateDefaultAccount();
+            bool result = account.Deposit(0);
+            Assert.IsFalse(result);
+            Assert.AreEqual(100, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Deposit_NegativeAmount_ReturnsFalse()
+        {
+            var account = CreateDefaultAccount();
+            bool result = account.Deposit(-10);
+            Assert.IsFalse(result);
+            Assert.AreEqual(100, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Deposit_MultipleDeposits_CorrectBalance()
+        {
+            var account = CreateDefaultAccount();
+            account.Deposit(50);
+            account.Deposit(25);
+            Assert.AreEqual(175, account.GetBalance());
+        }
 
         [TestMethod]
         public void Withdraw_ValidAmount()
@@ -37,9 +80,33 @@ namespace BankAccountAppTests
             Assert.IsTrue(result);
             Assert.AreEqual(60, account.GetBalance());
         }
-        // TODO: amount = 0 -> false
-        // TODO: amount > balance -> false, egyenleg változatlan
-        // TODO: pontosan az egyenleg összegének felvétele sikeres-e
+
+        [TestMethod]
+        public void Withdraw_ZeroAmount_ReturnsFalse()
+        {
+            var account = CreateDefaultAccount();
+            bool result = account.Withdraw(0);
+            Assert.IsFalse(result);
+            Assert.AreEqual(100, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_MoreThanBalance_ReturnsFalse()
+        {
+            var account = CreateDefaultAccount();
+            bool result = account.Withdraw(200);
+            Assert.IsFalse(result);
+            Assert.AreEqual(100, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_ExactBalance_Succeeds()
+        {
+            var account = CreateDefaultAccount();
+            bool result = account.Withdraw(100);
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, account.GetBalance());
+        }
 
         [TestMethod]
         public void Transfer_ValidAmount()
@@ -51,8 +118,25 @@ namespace BankAccountAppTests
             Assert.AreEqual(100, sender.GetBalance());
             Assert.AreEqual(150, receiver.GetBalance());
         }
-        // TODO: amount > sender balance -> false, mindkét egyenleg változatlan
-        // TODO: target = null -> false
+
+        [TestMethod]
+        public void Transfer_MoreThanBalance_ReturnsFalse()
+        {
+            var sender = new BankAccount("Alice", 200);
+            var receiver = new BankAccount("Bob", 50);
+            bool result = sender.Transfer(receiver, 500);
+            Assert.IsFalse(result);
+            Assert.AreEqual(200, sender.GetBalance());
+            Assert.AreEqual(50, receiver.GetBalance());
+        }
+
+        [TestMethod]
+        public void Transfer_NullTarget_ReturnsFalse()
+        {
+            var sender = new BankAccount("Alice", 200);
+            bool result = sender.Transfer(null, 100);
+            Assert.IsFalse(result);
+        }
     }
 
     [TestClass]
@@ -65,8 +149,38 @@ namespace BankAccountAppTests
             history.AddTransaction("Deposit", 100);
             Assert.AreEqual(1, history.GetTransactionCount());
         }
-        // TODO: null/üres type -> kivétel
-        // TODO: amount <= 0 -> kivétel
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddTransaction_NullType_ThrowsArgumentException()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction(null, 100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddTransaction_EmptyType_ThrowsArgumentException()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("", 100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddTransaction_ZeroAmount_ThrowsArgumentException()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("Deposit", 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddTransaction_NegativeAmount_ThrowsArgumentException()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("Deposit", -50);
+        }
 
         [TestMethod]
         public void GetLastTransaction_AfterOneDeposit()
@@ -75,8 +189,22 @@ namespace BankAccountAppTests
             history.AddTransaction("Deposit", 100);
             Assert.AreEqual("Deposit: 100.00", history.GetLastTransaction());
         }
-        // TODO: üres history -> null
-        // TODO: több tranzakció után csak az utolsót adja vissza
+
+        [TestMethod]
+        public void GetLastTransaction_EmptyHistory_ReturnsNull()
+        {
+            var history = new TransactionHistory();
+            Assert.IsNull(history.GetLastTransaction());
+        }
+
+        [TestMethod]
+        public void GetLastTransaction_MultipleTransactions_ReturnsLast()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("Deposit", 100);
+            history.AddTransaction("Withdrawal", 40);
+            Assert.AreEqual("Withdrawal: 40.00", history.GetLastTransaction());
+        }
 
         [TestMethod]
         public void GetTotalDeposited_MixedTransactions()
@@ -87,7 +215,14 @@ namespace BankAccountAppTests
             history.AddTransaction("Withdrawal", 30);
             Assert.AreEqual(150, history.GetTotalDeposited());
         }
-        // TODO: nincs deposit -> 0
+
+        [TestMethod]
+        public void GetTotalDeposited_NoDeposits_ReturnsZero()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("Withdrawal", 30);
+            Assert.AreEqual(0, history.GetTotalDeposited());
+        }
 
         [TestMethod]
         public void Clear_AfterTransactions()
@@ -97,8 +232,22 @@ namespace BankAccountAppTests
             history.Clear();
             Assert.AreEqual(0, history.GetTransactionCount());
         }
-        // TODO: Clear után GetLastTransaction() -> null
-        // TODO: üres history-n Clear() nem dob kivételt
+
+        [TestMethod]
+        public void Clear_GetLastTransaction_ReturnsNull()
+        {
+            var history = new TransactionHistory();
+            history.AddTransaction("Deposit", 100);
+            history.Clear();
+            Assert.IsNull(history.GetLastTransaction());
+        }
+
+        [TestMethod]
+        public void Clear_EmptyHistory_DoesNotThrow()
+        {
+            var history = new TransactionHistory();
+            history.Clear();
+            Assert.AreEqual(0, history.GetTransactionCount());
+        }
     }
 }
-
